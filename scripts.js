14123c2f3ba8cecd1b10f550fba8537d6177aefe
Daniel-Castro-1 <dc2288455@gmail.com>
2025-01-21 16:48:05 -0300

    consertado

--- scripts.js
+++ scripts.js
@@ -1,85 +1,66 @@
-const convertButton = document.querySelector("button")
-
+ const convertButton = document.querySelector(".convert-button")
 const currencySelect = document.querySelector(".currency-select")
 
-async function convertValues (){
+async function convertValues() {
     const inputCurrencyValue = document.querySelector(".input-currency").value
+    const MoedaValorConvert = document.querySelector(".valor-da-moeda-convert")
+    const moedaD = document.querySelector(".valor-da-moeda")
 
-const MoedaValorConvert = document.querySelector(".valor-da-moeda-convert")
+    const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then(resposta => resposta.json())
 
-const moedaD = document.querySelector(".valor-da-moeda")
+    const dolar = data.USDBRL.high
+    const euro = data.EURBRL.high
+    const bitcoin = data.BTCBRL.high
 
-const data  = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL").then( resposta =>  resposta.json())
-
-const dolar = data.USDBRL.higth
-
-const euro = data.EURBRL.higth
-
-const bitcoin = data.BTCBRL.high
-
-
-
-if(currencySelect.value == "dolar"){
-    moedaD.innerHTML = new Intl.NumberFormat("en-US",{
-        style: "currency",
-        currency:"USD"
-    
-    }).format( inputCurrencyValue / dolar)
-
-}
-
-
-if(currencySelect.value == "euro"){
- moedaD.innerHTML = new Intl.NumberFormat("de-DE",{
-     style: "currency",
-     currency:"EUR"
-      
-
-    }).format(inputCurrencyValue / euro )
-
-}
-
-
-if(currencySelect.value == "Bitcoin"){
-    moedaD.innerHTML = new Intl.NumberFormat("eu-SP",{
-    style: "currency",
-    currency:"BTC"
-}).format(inputCurrencyValue / bitcoin )
-
-
-
-}
-
-
-
-MoedaValorConvert.innerHTML = new Intl.NumberFormat("pt-BR",{
-    style: "currency",
-    currency: "BRL"
-}).format(inputCurrencyValue)
-
-}
-
-
-function changeCurrency (){
-    const currencyNAme = document.querySelector("#currency-name")
-    const currencyImg = document.querySelector(".currency-img")
-    if(currencySelect.value == "dolar"){
-        currencyNAme.innerHTML= "Dólar Americano"
-       currencyImg.src = "./assets/dolar.png"   
+    if (currencySelect.value == "dolar") {
+        moedaD.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dolar)
     }
 
-    if(currencySelect.value == "euro"){
-        currencyNAme.innerHTML = "Euro" 
-        currencyImg.src = './assets/euro.png'  
+    if (currencySelect.value == "euro") {
+        moedaD.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputCurrencyValue / euro)
+    }
+
+    if (currencySelect.value == "Bitcoin") {
+        moedaD.innerHTML = new Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "BTC"
+        }).format(inputCurrencyValue / bitcoin)
+    }
+
+    MoedaValorConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue)
 }
 
-if(currencySelect.value == "Bitcoin"){
-    currencyNAme.innerHTML = "Bitcoin"
-    currencyImg.src = "./assets/bitcoin.png"
+function changeCurrency() {
+    const currencyNAme = document.querySelector("#currency-name")
+    const currencyImg = document.querySelector(".currency-img")
+    if (currencySelect.value == "dolar") {
+        currencyNAme.innerHTML = "Dólar Americano"
+        currencyImg.src = "./assets/dolar.png"
+    }
+
+    if (currencySelect.value == "euro") {
+        currencyNAme.innerHTML = "Euro"
+        currencyImg.src = './assets/euro.png'
+    }
+
+    if (currencySelect.value == "Bitcoin") {
+        currencyNAme.innerHTML = "Bitcoin"
+        currencyImg.src = "./assets/bitcoin.png"
+    }
+
+    convertValues()
 }
 
-convertValues()
-}
-
-currencySelect.addEventListener("change",changeCurrency  )
+currencySelect.addEventListener("change", changeCurrency)
 convertButton.addEventListener("click", convertValues)
+ 
+
